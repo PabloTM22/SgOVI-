230f7027f2969214def29efce39b2b6932b640a4
PabloTM <pablo.torrentem@gmail.com>
2026-03-04 16:12:20 +0100

    primer commit + USR01

---
+++ src/main/java/es/uji/ei1027/sgovi/SgOviApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.sgovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgOviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgOviApplication.class, args);
+    }
+
+}
