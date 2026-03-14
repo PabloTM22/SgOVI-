7099c762b605692e5b0b8557ae14e33303d228f1
rabalquique <al426222@uji.es>
2026-03-14 11:27:30 +0100

    SgOviApplication corregido

--- src/main/java/es/uji/ei1027/sgovi/SgOviApplication.java
+++ src/main/java/es/uji/ei1027/sgovi/SgOviApplication.java
@@ -1,13 +1,24 @@
 package es.uji.ei1027.sgovi;
 
-import org.springframework.boot.SpringApplication;
+import java.util.logging.Logger;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SgOviApplication {
+public class SgOviApplication implements CommandLineRunner {
+
+    private static final Logger log = Logger.getLogger(SgOviApplication.class.getName());
 
     public static void main(String[] args) {
-        SpringApplication.run(SgOviApplication.class, args);
+        // Auto-configura l'aplicació
+        new SpringApplicationBuilder(SgOviApplication.class).run(args);
     }
 
+    // Funció principal
+    public void run(String... strings) throws Exception {
+        log.info("Ací va el meu codi");
+    }
 }
+
