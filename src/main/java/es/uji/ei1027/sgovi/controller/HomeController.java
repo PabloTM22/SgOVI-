45b137ba6a16c0c27fb3f36c30dfcce0dfed19b6
rabalquique <al426222@uji.es>
2026-03-14 16:31:28 +0100

    Creacion layout base y HomeController

---
+++ src/main/java/es/uji/ei1027/sgovi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.sgovi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
