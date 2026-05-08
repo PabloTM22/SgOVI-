e191921a38a800c22c4f12e82b4540c5d30eddae
FelipeBolumar <al415731@uji.es>
2026-05-08 10:06:39 +0200

    Correccion CandidatoRowMapper tras ejecucion

--- src/main/java/es/uji/ei1027/sgovi/dao/CandidatoRowMapper.java
+++ src/main/java/es/uji/ei1027/sgovi/dao/CandidatoRowMapper.java
@@ -23,9 +23,10 @@ public class CandidatoRowMapper implements RowMapper<Candidato> {
         candidato.setExperiencia(rs.getString("experiencia"));
         candidato.setDisponibilidad(rs.getString("disponibilidad"));
 
-        // Usamos getObject para que si en la BD es NULL, en Java sea null y no 0.0
-        candidato.setLatitud(rs.getObject("latitud", Double.class));
-        candidato.setLongitud(rs.getObject("longitud", Double.class));
+        double lat = rs.getDouble("latitud");
+        candidato.setLatitud(rs.wasNull() ? null : lat);
+        double lng = rs.getDouble("longitud");
+        candidato.setLongitud(rs.wasNull() ? null : lng);
 
         candidato.setEstadoAceptado(rs.getBoolean("estado_aceptado"));
         candidato.setContrasena(rs.getString("contrasena"));
