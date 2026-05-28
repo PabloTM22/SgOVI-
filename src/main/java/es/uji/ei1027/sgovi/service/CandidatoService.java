0a0022eb2065142bec5a0a34a1b68fbada271bd3
PabloTM <pablo.torrentem@gmail.com>
2026-05-28 18:13:16 +0200

    modificado CandidatoService:
    Encriptamos contraseñas

--- src/main/java/es/uji/ei1027/sgovi/service/CandidatoService.java
+++ src/main/java/es/uji/ei1027/sgovi/service/CandidatoService.java
@@ -2,6 +2,7 @@ package es.uji.ei1027.sgovi.service;
 
 import es.uji.ei1027.sgovi.dao.CandidatoDao;
 import es.uji.ei1027.sgovi.model.Candidato;
+import org.jasypt.util.password.BasicPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,20 +18,15 @@ public class CandidatoService {
         this.candidatoDao = candidatoDao;
     }
 
-    // Método que llamaremos cuando alguien rellene el formulario web
     public void registrarCandidato(Candidato candidato) {
-
-        // 1. Generamos un ID único para el candidato (ej: AP-a1b2c3d4)
         String idAp = "AP-" + UUID.randomUUID().toString().substring(0, 8);
         candidato.setIdAp(idAp);
-
-        // 2. Lógica de negocio: Un candidato nuevo NO está aceptado por defecto
         candidato.setEstadoAceptado(false);
-
-        // 3. Lógica de negocio: El perfil nace activo en el sistema
         candidato.setActivo(true);
 
-        // 4. Por último, lo guardamos en la base de datos usando el DAO que hiciste antes
+        BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
+        candidato.setContrasena(encryptor.encryptPassword(candidato.getContrasena()));
+
         candidatoDao.addCandidato(candidato);
     }
 }
