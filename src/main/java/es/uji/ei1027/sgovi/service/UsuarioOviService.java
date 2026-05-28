d05cd5356402aed23a2aa3b310c9dbee95acaa6f
PabloTM <pablo.torrentem@gmail.com>
2026-05-28 18:10:42 +0200

    modificado archivo de UsuarioOviServiceService

--- src/main/java/es/uji/ei1027/sgovi/service/UsuarioOviService.java
+++ src/main/java/es/uji/ei1027/sgovi/service/UsuarioOviService.java
@@ -2,6 +2,7 @@ package es.uji.ei1027.sgovi.service;
 
 import es.uji.ei1027.sgovi.dao.UsuarioOviDao;
 import es.uji.ei1027.sgovi.model.UsuarioOvi;
+import org.jasypt.util.password.BasicPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,17 +18,14 @@ public class UsuarioOviService {
         this.usuarioOviDao = usuarioOviDao;
     }
 
-    // Método que se llama cuando un usuario OVI se registra
     public void registrarUsuario(UsuarioOvi usuario) {
-
-        // 1. Generamos un ID único para el usuario (ej: usr_a1b2c3d4)
         String idUsuario = "usr_" + UUID.randomUUID().toString().substring(0, 8);
         usuario.setIdUsuario(idUsuario);
-
-        // 2. Lógica de negocio: un usuario nuevo no está aceptado por defecto
         usuario.setAceptadoTecnico(false);
 
-        // 3. Guardamos en la base de datos usando el DAO
+        BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
+        usuario.setContrasena(encryptor.encryptPassword(usuario.getContrasena()));
+
         usuarioOviDao.addUsuario(usuario);
     }
 }
