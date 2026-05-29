c020ae1a0f7857fd8af829be0b0f52a554e2e097
FelipeBolumar <al415731@uji.es>
2026-05-29 20:06:46 +0200

    Cambiado UsuarioOviService

--- src/main/java/es/uji/ei1027/sgovi/service/UsuarioOviService.java
+++ src/main/java/es/uji/ei1027/sgovi/service/UsuarioOviService.java
@@ -1,6 +1,8 @@
 package es.uji.ei1027.sgovi.service;
 
+import es.uji.ei1027.sgovi.dao.UsuarioDao;
 import es.uji.ei1027.sgovi.dao.UsuarioOviDao;
+import es.uji.ei1027.sgovi.model.Usuario;
 import es.uji.ei1027.sgovi.model.UsuarioOvi;
 import org.jasypt.util.password.BasicPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,31 +14,43 @@ import java.util.UUID;
 public class UsuarioOviService {
 
     private final UsuarioOviDao usuarioOviDao;
+    private final UsuarioDao usuarioDao;
 
     @Autowired
-    public UsuarioOviService(UsuarioOviDao usuarioOviDao) {
+    public UsuarioOviService(UsuarioOviDao usuarioOviDao, UsuarioDao usuarioDao) {
         this.usuarioOviDao = usuarioOviDao;
+        this.usuarioDao = usuarioDao;
     }
 
-    public void registrarUsuario(UsuarioOvi usuario) {
+    public void registrarUsuario(UsuarioOvi usuarioOvi) {
         String idUsuario = "usr_" + UUID.randomUUID().toString().substring(0, 8);
-        usuario.setIdUsuario(idUsuario);
-        usuario.setAceptadoTecnico(false);
+        usuarioOvi.setIdUsuario(idUsuario);
+        usuarioOvi.setAceptadoTecnico(false);
 
         BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
-        usuario.setContrasena(encryptor.encryptPassword(usuario.getContrasena()));
+        String hash = encryptor.encryptPassword(usuarioOvi.getContrasena());
 
-        usuarioOviDao.addUsuario(usuario);
+        Usuario credenciales = new Usuario();
+        credenciales.setUsername(idUsuario);
+        credenciales.setPassword(hash);
+        credenciales.setRol("USUARIO_OVI");
+        credenciales.setActivo(false);
+        usuarioDao.addUsuario(credenciales);
+
+        usuarioOvi.setContrasena(hash);
+        usuarioOviDao.addUsuario(usuarioOvi);
     }
 
-    public void actualizarUsuario(UsuarioOvi usuario) {
-        UsuarioOvi actual = usuarioOviDao.getUsuario(usuario.getIdUsuario());
-        if (usuario.getContrasena() == null || usuario.getContrasena().isBlank()) {
-            usuario.setContrasena(actual.getContrasena());
+    public void actualizarUsuario(UsuarioOvi usuarioOvi) {
+        UsuarioOvi actual = usuarioOviDao.getUsuario(usuarioOvi.getIdUsuario());
+        if (usuarioOvi.getContrasena() == null || usuarioOvi.getContrasena().isBlank()) {
+            usuarioOvi.setContrasena(actual.getContrasena());
         } else {
             BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
-            usuario.setContrasena(encryptor.encryptPassword(usuario.getContrasena()));
+            String hash = encryptor.encryptPassword(usuarioOvi.getContrasena());
+            usuarioOvi.setContrasena(hash);
+            usuarioDao.updatePassword(usuarioOvi.getIdUsuario(), hash);
         }
-        usuarioOviDao.updateUsuario(usuario);
+        usuarioOviDao.updateUsuario(usuarioOvi);
     }
 }
