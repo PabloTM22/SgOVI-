735254c15c8f5b84f9f3654b2278993d86c0b823
FelipeBolumar <al415731@uji.es>
2026-05-31 09:27:05 +0200

    Creacion paquete interceptor + clase IdentificacionInterceptor

---
+++ src/main/java/es/uji/ei1027/sgovi/interceptor/IdentificacionInterceptor.java
@@ -0,0 +1,30 @@
+package es.uji.ei1027.sgovi.interceptor;
+
+import es.uji.ei1027.sgovi.model.UserDetails;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class IdentificacionInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        UserDetails user = (UserDetails) session.getAttribute("user");
+        if (user != null) {
+            return true;
+        }
+        session.setAttribute("nextUrl", urlSolicitada(request));
+        response.sendRedirect(request.getContextPath() + "/login");
+        return false;
+    }
+
+    private String urlSolicitada(HttpServletRequest request) {
+        String uri = request.getRequestURI().substring(request.getContextPath().length());
+        String query = request.getQueryString();
+        return query == null ? uri : uri + "?" + query;
+    }
+}
