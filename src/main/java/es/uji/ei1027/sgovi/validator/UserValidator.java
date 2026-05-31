c792c51b680a7f45bd1a5e7f0e74873510a159bf
FelipeBolumar <al415731@uji.es>
2026-05-31 09:31:20 +0200

    Cambiado UserValidator a Component

--- src/main/java/es/uji/ei1027/sgovi/validator/UserValidator.java
+++ src/main/java/es/uji/ei1027/sgovi/validator/UserValidator.java
@@ -1,10 +1,13 @@
 package es.uji.ei1027.sgovi.validator;
 
 import es.uji.ei1027.sgovi.model.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+@Component
 public class UserValidator implements Validator {
+
     @Override
     public boolean supports(Class<?> cls) {
         return UserDetails.class.isAssignableFrom(cls);
@@ -14,10 +17,10 @@ public class UserValidator implements Validator {
     public void validate(Object obj, Errors errors) {
         UserDetails user = (UserDetails) obj;
         if (user.getUsername() == null || user.getUsername().trim().isEmpty()) {
-            errors.rejectValue("username", "required", "El nombre de usuario es obligatorio");
+            errors.rejectValue("username", "required", "El usuario es obligatorio.");
         }
         if (user.getPassword() == null || user.getPassword().trim().isEmpty()) {
-            errors.rejectValue("password", "required", "La contraseña es obligatoria");
+            errors.rejectValue("password", "required", "La contraseña es obligatoria.");
         }
     }
 }
